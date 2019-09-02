586a3554d255a09b9dc32f2db20773ace3d40aae
apsarams <36523918+apsarams@users.noreply.github.com>
2019-09-02 12:57:32 +0200

    Update ShoppingCartItem.java

--- src/ShoppingCartItem.java
+++ src/ShoppingCartItem.java
@@ -19,32 +19,20 @@ public class ShoppingCartItem implements Cloneable
 		this.descriptionURL = descriptionURL;
 	}
 
-// The add method is a quick method for combining two similar
-// items.
-
 	public void add(ShoppingCartItem otherItem)
 	{
 		this.quantity = this.quantity + otherItem.quantity;
 	}
-
-// The subtract method is similar to the add method, but it
-// removes a certain quantity of items.
 
 	public void subtract(ShoppingCartItem otherItem)
 	{
 		this.quantity = this.quantity - otherItem.quantity;
 	}
 
-// You can store items in a hash table if you implement hashCode. It's
-// always a good idea to do this.
-
 	public int hashCode()
 	{
 		return itemName.hashCode() + itemCost;
 	}
-
-// it only
-// compares the item names and item costs.
 
 	public boolean equals(Object other)
 	{
@@ -58,14 +46,10 @@ public class ShoppingCartItem implements Cloneable
 		return (itemName.equals(otherItem.itemName)) && (itemCost == otherItem.itemCost);
 	}
 
-// Create a copy of this object
-
 	public ShoppingCartItem copy()
 	{
 		return new ShoppingCartItem(itemName, itemCost, quantity, descriptionURL);
 	}
-
-// Create a printable version of this object
 
 	public String toString()
 	{
